6983f41ad9291c140de1e8b8370f5a76979db0f5
Mansur Isaev <737dab2f169a@mail.ru>
2023-12-11 09:19:05 +0400

    Add `HasCommand` & `RemoveCommand`
    Closes #7

--- scripts/ConsoleMono.cs
+++ scripts/ConsoleMono.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Reflection.Metadata;
 using Godot;
 
 public partial class ConsoleMono : Node
@@ -12,17 +13,27 @@ public partial class ConsoleMono : Node
 		_console = GetNode("/root/Console");
 	}
 
+	public static bool HasCommand(StringName command)
+	{
+		return (bool)_console.Call("has_command", command);
+	}
+
 	public static void CreateCommand(StringName command, Delegate callable, StringName description = null)
 	{
 		object? target = callable.Target;
 		if (target is not GodotObject godotObject)
 			throw new ArgumentException("Class does not inherits GameObject or method is static");
-		_console.Call("create_command", command, 
-			new Callable(godotObject, callable.Method.Name), description??"");
+		_console.Call("create_command", command,
+			new Callable(godotObject, callable.Method.Name), description ?? "");
 	}
-	public static void CreateCommand(StringName command, GodotObject target, StringName callable, StringName description=null)
+	public static void CreateCommand(StringName command, GodotObject target, StringName callable, StringName description = null)
 	{
-		_console.Call("create_command", command, new Callable(target, callable), description??"");
+		_console.Call("create_command", command, new Callable(target, callable), description ?? "");
+	}
+
+	public static bool RemoveCommand(StringName command)
+	{
+		return (bool)_console.Call("remove_command", command);
 	}
 
 	public static void Print(string line)
